63b7191e4362ef8a32b44a07ede6276c07800d21
MurituMaina <vincent.maina@student.moringaschool.com>
2022-10-24 21:59:24 +0300

    commit tree 890c94cb8f3f7b19038a29b6aeb8c2a3f14e6f02

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,12 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+
+class Car < Vehicle
+def wheel_number
+    @wheel_number
+end
+def go
+     return 'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
+end
+
+end
