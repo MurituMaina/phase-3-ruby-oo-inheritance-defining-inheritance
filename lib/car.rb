31c470ff0b1026ea051cb7e66d4f7af77798c933
MurituMaina <vincent.maina@student.moringaschool.com>
2022-10-24 22:03:29 +0300

    completed lesson

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,14 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+require 'pry'
+
+class Car < Vehicle
+def wheel_number
+    @wheel_number
+end
+def go
+     return 'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
+end
+
+end
+binding.pry
